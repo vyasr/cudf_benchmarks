a19a82ff2dd3a49cd4ef3446b47acb4d781fb01a
Vyas Ramasubramani <vyasr@nvidia.com>
2021-07-09 10:15:19 -0700

    Move make_frame to utils.

--- bench_merge.py
+++ bench_merge.py
@@ -1,24 +1,5 @@
-import cudf
-import cupy as cp
+import utils
 import pytest
-
-# low = 0
-# high = 100
-# nkey_cols = 2
-# nrows =
-
-
-def make_frame(ncols, nkey_cols, nrows, low=0, high=100):
-    nval_cols = ncols - nkey_cols
-    key_columns = {
-        f"key{i}": cp.random.randint(low, high, nrows)
-        for i in range(nkey_cols)
-    }
-    val_columns = {
-        f"val{i}": cp.random.rand(nrows)
-        for i in range(nval_cols)
-    }
-    return cudf.DataFrame({**key_columns, **val_columns})
 
 
 @pytest.mark.parametrize(
@@ -31,6 +12,6 @@ def make_frame(ncols, nkey_cols, nrows, low=0, high=100):
     'nrows', [10, 100, 1000, 10_000, 100_000, 1_000_000],
 )
 def test_merge(benchmark, ncols, nkey_cols, nrows):
-    lhs = make_frame(ncols, nkey_cols, nrows)
-    rhs = make_frame(ncols, nkey_cols, nrows // 2)
+    lhs = utils.make_frame(ncols, nkey_cols, nrows)
+    rhs = utils.make_frame(ncols, nkey_cols, nrows // 2)
     benchmark(lhs.merge, rhs, on=[f"key{i}" for i in range(nkey_cols)])
