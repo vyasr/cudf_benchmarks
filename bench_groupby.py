ef702ebce3b22c154c784fa83b6fb20091cdc2d4
Vyas Ramasubramani <vyasr@nvidia.com>
2021-07-09 10:19:48 -0700

    Add groupby benchmarks.

---
+++ bench_groupby.py
@@ -0,0 +1,16 @@
+import utils
+import pytest
+
+
+@pytest.mark.parametrize(
+    'ncols', [4, 7, 10],
+)
+@pytest.mark.parametrize(
+    'nkey_cols', [2, 3, 4],
+)
+@pytest.mark.parametrize(
+    'nrows', [100, 10_000, 1_000_000],
+)
+def test_groupby(benchmark, ncols, nkey_cols, nrows):
+    frame = utils.make_frame(ncols, nkey_cols, nrows)
+    benchmark(frame.groupby, by=[f"key{i}" for i in range(nkey_cols)])
