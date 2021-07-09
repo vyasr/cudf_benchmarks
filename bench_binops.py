73a35ce6b56a7b1615e34ec3fa9a2aacab88e2e5
Vyas Ramasubramani <vyasr@nvidia.com>
2021-07-09 09:55:44 -0700

    Add support for nulls and DataFrame.

--- bench_binops.py
+++ bench_binops.py
@@ -8,11 +8,18 @@ import pytest
     'op', [operator.add, operator.mul, operator.__and__, operator.eq],
 )
 @pytest.mark.parametrize(
-    'frame_type', [cudf.Series, cudf.Index]
+    'cls', [cudf.Series, cudf.Index, cudf.DataFrame]
 )
 @pytest.mark.parametrize(
     'N', [1_000, 100_000, 10_000_000]
 )
-def test_binops(benchmark, op, frame_type, N):
-    frame = frame_type(cp.random.rand(N))
+@pytest.mark.parametrize(
+    'has_nulls', [False, True]
+)
+def test_binops(benchmark, op, cls, N, has_nulls):
+    # Need int array for __and__
+    ser = cudf.Series(cp.random.rand(N), dtype=int)
+    if has_nulls:
+        ser[::2] = None
+    frame = cls({'a': ser}) if cls is cudf.DataFrame else cls(ser)
     benchmark(lambda: op(frame, frame))
