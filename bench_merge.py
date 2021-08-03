273f7664066734509b2b42a62eda0fe91c7e5688
Vyas Ramasubramani <vyasr@nvidia.com>
2021-08-03 16:33:13 -0700

    Reduce number of merge benchmarks.

--- bench_merge.py
+++ bench_merge.py
@@ -9,7 +9,7 @@ import pytest
     'nkey_cols', [2, 3, 4],
 )
 @pytest.mark.parametrize(
-    'nrows', [10, 100, 1000, 10_000, 100_000, 1_000_000],
+    'nrows', [100, 10_000, 1_000_000],
 )
 def test_merge(benchmark, ncols, nkey_cols, nrows):
     lhs = utils.make_frame(ncols, nkey_cols, nrows)
