863f058232e5af53c57e2f6da48cb75bd263bccb
Vyas Ramasubramani <vyasr@nvidia.com>
2021-08-11 17:25:27 -0700

    Benchmark scan ops.

---
+++ bench_scans.py
@@ -0,0 +1,24 @@
+import cudf
+import cupy as cp
+import pytest
+
+
+@pytest.mark.parametrize(
+    'op', ['cumsum', 'cumprod', 'cummax'],
+)
+@pytest.mark.parametrize(
+    'cls', [cudf.Series, cudf.DataFrame]
+)
+@pytest.mark.parametrize(
+    'N', [1_000, 100_000, 10_000_000]
+)
+@pytest.mark.parametrize(
+    'has_nulls', [False, True]
+)
+def test_scans(benchmark, op, cls, N, has_nulls):
+    # Need int array for __and__
+    ser = cudf.Series(cp.random.rand(N), dtype=int)
+    if has_nulls:
+        ser[::2] = None
+    frame = cls({'a': ser}) if cls is cudf.DataFrame else cls(ser)
+    benchmark(lambda: getattr(frame, op)())
