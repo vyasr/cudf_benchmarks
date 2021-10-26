22b7606c46a49998c79b87ece3c3b707dd7e48e5
Michael Wang <isVoid@users.noreply.github.com>
2021-10-26 15:01:30 -0700

    Remove numpy import; remove lambda wrap

--- bench_get_dummies.py
+++ bench_get_dummies.py
@@ -1,29 +1,37 @@
 import cudf
 import cupy as cp
-import numpy as np
 
 import pytest
 
 
 @pytest.mark.parametrize(
-    "size", [10_000, 100_000, 1_000_000]
+    "size", [10_000, 100_000]
 )
 @pytest.mark.parametrize(
     "cardinality", [10, 100, 1000]
 )
 @pytest.mark.parametrize(
-    "dtype", [np.bool_, np.float64]
+    "dtype", [cp.bool_, cp.float64]
 )
-def test_get_dummies(benchmark, size, cardinality, dtype):
-    df = cudf.DataFrame({'col': cudf.Series(
-        cp.random.randint(low=0, high=cardinality, size=size)
-    ).astype("category")})
-    benchmark(lambda: cudf.get_dummies(df, columns=['col'], dtype=dtype))
+def test_get_dummies_high_cardinality(benchmark, size, cardinality, dtype):
+    """This test is mean to test the performance of get_dummies given the
+    cardinality of column to encode is high.
+    """
+    df = cudf.DataFrame(
+            {'col': cudf.Series(
+                        cp.random.randint(low=0, high=cardinality, size=size)
+                    ).astype("category")
+            }
+        )
+    benchmark(cudf.get_dummies, df, columns=['col'], dtype=dtype)
 
 @pytest.mark.parametrize(
     "prefix", [None, 'pre']
 )
 def test_get_dummies_simple(benchmark, prefix):
+    """This test provides a small input to get_dummies to test the efficiency
+    of the API itself.
+    """
     df = cudf.DataFrame(
             {
                 'col1': list(range(10)),
@@ -31,17 +39,4 @@ def test_get_dummies_simple(benchmark, prefix):
                 'col3': cudf.Series(list(range(100, 110)), dtype='category')
             }
         )
-    benchmark(lambda: cudf.get_dummies(df, columns=['col1', 'col2', 'col3'], prefix=prefix))
-
-# The below was used to do detail profiling in nvtx
-# import nvtx
-
-# df = cudf.DataFrame({'col': cudf.Series(
-#     cp.random.randint(low=0, high=1000, size=1_000_000)
-# ).astype("category")})
-
-# s = cudf.Series([1])
-# s2 = cudf.Series([2])
-
-# with nvtx.annotate("benchmark_all"):
-#     res = cudf.concat([s, s2])
+    benchmark(cudf.get_dummies, df, columns=['col1', 'col2', 'col3'], prefix=prefix)
