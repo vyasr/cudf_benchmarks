f2d3e778f2c53a7b8ed97f34f68d920dffebe56c
Michael Wang <michaelwang0905@icloud.com>
2021-12-01 11:23:09 -0800

    Avoid using global variables in conftest.py

--- conftest.py
+++ conftest.py
@@ -4,15 +4,20 @@ import pytest
 
 from utils import make_col, make_frame
 
-n = [100, 10000]
-has_nulls = [True, False]
 
-
-@pytest.fixture(params=product(n, has_nulls))
+@pytest.fixture(params=product([100, 10000], [True, False]))
 def col(request):
+    """Create a cudf column.
+
+    The two parameters are `nrows` and `has_nulls`
+    """
     return make_col(*request.param)
 
 
-@pytest.fixture(params=n)
+@pytest.fixture(params=[100, 10000])
 def df(request):
+    """Create a cudf DataFrame.
+
+    The two parameters are `nrows`
+    """
     return make_frame(ncols=5, nkey_cols=0, nrows=request.param)
