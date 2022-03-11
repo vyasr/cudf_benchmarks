9d9644eaa5301df7894c2fe4b1ba317396240518
isVoid <michaelwang0905@icloud.com>
2022-03-11 12:45:02 -0800

    groupby agg benchmark

--- bench_groupby.py
+++ bench_groupby.py
@@ -18,3 +18,25 @@ import utils
 def test_groupby(benchmark, ncols, nkey_cols, nrows):
     frame = utils.make_frame(ncols, nkey_cols, nrows)
     benchmark(frame.groupby, by=[f"key{i}" for i in range(nkey_cols)])
+
+
+@pytest.mark.parametrize(
+    "nrows",
+    [100, 10_000, 1_000_000],
+)
+@pytest.mark.parametrize(
+    "agg",
+    ["sum", ["sum", "mean"], {f"val{i}": ["sum", "mean", "count"] for i in range(6)}],
+)
+@pytest.mark.parametrize("as_index", [True, False])
+@pytest.mark.parametrize("sort", [True, False])
+def test_groupby_agg(benchmark, nrows, agg, as_index, sort):
+    ncols = 10
+    nkey_cols = 4
+    frame = utils.make_frame(ncols, nkey_cols, nrows)
+    benchmark(
+        frame.groupby(
+            by=[f"key{i}" for i in range(nkey_cols)], as_index=as_index, sort=sort
+        ).agg,
+        agg,
+    )
