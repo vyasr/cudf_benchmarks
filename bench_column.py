042390cfb44529c4affc43449a2ebbabe4c63c9f
Sheilah <kirui.sheilah@gmail.com>
2022-05-03 14:18:52 -0700

    added parameters to where

--- bench_column.py
+++ bench_column.py
@@ -5,10 +5,10 @@ import pytest
 @pytest.mark.parametrize("N", [1_000, 100_000, 10_000_000])
 def test_rangeindex_column(benchmark, N):
     obj = cudf.RangeIndex(range(N))
-    benchmark(obj._column)
+    benchmark(lambda: obj._column)
 
 
 @pytest.mark.parametrize("N", [1_000, 100_000, 10_000_000])
 def test_rangeindex_columns(benchmark, N):
     obj = cudf.RangeIndex(range(N))
-    benchmark(obj._columns)
+    benchmark(lambda: obj._columns)
