9cad84a3ecd682c093a4ae6ad77bfd28e65127e2
Sheilah <kirui.sheilah@gmail.com>
2022-05-03 14:18:52 -0700

    added parameters to where

--- bench_where.py
+++ bench_where.py
@@ -5,4 +5,5 @@ import pytest
 @pytest.mark.parametrize("N", [1_000, 100_000, 10_000_000])
 def test_rangeindex_where(benchmark, N):
     obj = cudf.RangeIndex(range(N))
-    benchmark(obj.where)
+    cond = obj % 2 == 0
+    benchmark(obj.where, cond, 0)
