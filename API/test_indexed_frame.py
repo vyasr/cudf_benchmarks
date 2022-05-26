c941ae5ce7ce9187cd316b3ac1862a78cf2302a1
Vyas Ramasubramani <vyasr@nvidia.com>
2022-05-26 13:22:36 -0700

    Merge reduction benchmarks into indexed_frame.py.

--- API/test_indexed_frame.py
+++ API/test_indexed_frame.py
@@ -4,3 +4,8 @@ import pytest
 @pytest.mark.parametrize("op", ["cumsum", "cumprod", "cummax"])
 def test_scans(benchmark, op, indexed_frame):
     benchmark(getattr(indexed_frame, op))
+
+
+@pytest.mark.parametrize("op", ["sum", "product", "mean"])
+def test_reductions(benchmark, op, indexed_frame):
+    benchmark(getattr(indexed_frame, op))
