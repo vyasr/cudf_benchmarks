a697b4b11ec3c31ee692598d305b84563e3de50e
Vyas Ramasubramani <vyasr@nvidia.com>
2022-06-08 17:39:39 -0700

    Convert almost all dataframe benchmarks except sampling.

--- API/bench_dataframe.py
+++ API/bench_dataframe.py
@@ -11,28 +11,26 @@ def bench_construction(benchmark, N):
     benchmark(cudf.DataFrame, {None: cupy.random.rand(N)})
 
 
+@cudf_benchmark(cls="dataframe", dtype="float", cols=6)
 @pytest.mark.parametrize(
     "expr", ["a+b", "a+b+c+d+e", "a / (sin(a) + cos(b)) * tan(d*e*f)"]
 )
-def bench_eval_func(benchmark, expr, dataframe_dtype_float_cols_6):
-    benchmark(dataframe_dtype_float_cols_6.eval, expr)
+def bench_eval_func(benchmark, expr, dataframe):
+    benchmark(dataframe.eval, expr)
 
 
+@cudf_benchmark(cls="dataframe", dtype="int", nulls=False, cols=6, name="df")
 @pytest.mark.parametrize(
     "nkey_cols",
     [2, 3, 4],
 )
-def bench_merge(benchmark, dataframe_dtype_int_nulls_false_cols_6, nkey_cols):
-    on = list(dataframe_dtype_int_nulls_false_cols_6.columns[:nkey_cols])
-    benchmark(
-        dataframe_dtype_int_nulls_false_cols_6.merge,
-        dataframe_dtype_int_nulls_false_cols_6,
-        on=on,
-    )
+def bench_merge(benchmark, df, nkey_cols):
+    benchmark(df.merge, df, on=list(df.columns[:nkey_cols]))
 
 
 # TODO: Some of these cases could be generalized to an IndexedFrame benchmark
 # instead of a DataFrame benchmark.
+@cudf_benchmark(cls="dataframe", dtype="int")
 @pytest.mark.parametrize(
     "values",
     [
@@ -42,8 +40,8 @@ def bench_merge(benchmark, dataframe_dtype_int_nulls_false_cols_6, nkey_cols):
         cudf.Series(range(1000)),
     ],
 )
-def bench_isin(benchmark, dataframe_dtype_int, values):
-    benchmark(dataframe_dtype_int.isin, values)
+def bench_isin(benchmark, dataframe, values):
+    benchmark(dataframe.isin, values)
 
 
 @pytest.fixture(
@@ -64,15 +62,16 @@ def bench_sample(benchmark, dataframe_dtype_int, axis, frac, random_state):
     )
 
 
+@cudf_benchmark(cls="dataframe", dtype="int", nulls=False, cols=6)
 @pytest.mark.parametrize(
     "nkey_cols",
     [2, 3, 4],
 )
-def bench_groupby(benchmark, dataframe_dtype_int_nulls_false_cols_6, nkey_cols):
-    by = list(dataframe_dtype_int_nulls_false_cols_6.columns[:nkey_cols])
-    benchmark(dataframe_dtype_int_nulls_false_cols_6.groupby, by=by)
+def bench_groupby(benchmark, dataframe, nkey_cols):
+    benchmark(dataframe.groupby, by=list(dataframe.columns[:nkey_cols]))
 
 
+@cudf_benchmark(cls="dataframe", dtype="int", nulls=False, cols=6)
 @pytest.mark.parametrize(
     "agg",
     [
@@ -87,34 +86,20 @@ def bench_groupby(benchmark, dataframe_dtype_int_nulls_false_cols_6, nkey_cols):
 )
 @pytest.mark.parametrize("as_index", [True, False])
 @pytest.mark.parametrize("sort", [True, False])
-def bench_groupby_agg(
-    benchmark, dataframe_dtype_int_nulls_false_cols_6, agg, nkey_cols, as_index, sort
-):
-    by = list(dataframe_dtype_int_nulls_false_cols_6.columns[:nkey_cols])
-    benchmark(
-        dataframe_dtype_int_nulls_false_cols_6.groupby(
-            by=by, as_index=as_index, sort=sort
-        ).agg,
-        agg,
-    )
+def bench_groupby_agg(benchmark, dataframe, agg, nkey_cols, as_index, sort):
+    by = list(dataframe.columns[:nkey_cols])
+    benchmark(dataframe.groupby(by=by, as_index=as_index, sort=sort).agg, agg)
 
 
+@cudf_benchmark(cls="dataframe", dtype="int")
 @pytest.mark.parametrize("ncol_sort", [1])
-def bench_sort_values(benchmark, dataframe_dtype_int, ncol_sort):
-    by = list(dataframe_dtype_int.columns[:ncol_sort])
-    benchmark(dataframe_dtype_int.sort_values, by)
+def bench_sort_values(benchmark, dataframe, ncol_sort):
+    benchmark(dataframe.sort_values, list(dataframe.columns[:ncol_sort]))
 
 
+@cudf_benchmark(cls="dataframe", dtype="int")
 @pytest.mark.parametrize("ncol_sort", [1])
 @pytest.mark.parametrize("n", [10])
-def bench_nsmallest(benchmark, dataframe_dtype_int, ncol_sort, n):
-    by = list(dataframe_dtype_int.columns[:ncol_sort])
-    benchmark(dataframe_dtype_int.nsmallest, n, by)
-
-
-@cudf_benchmark(cls="dataframe", dtype="int", cols=6)
-@pytest.mark.parametrize(
-    "expr", ["a+b", "a+b+c+d+e", "a / (sin(a) + cos(b)) * tan(d*e*f)"]
-)
-def bench_eval_func2(benchmark, dataframe, expr):
-    benchmark(dataframe.eval, "a+b")
+def bench_nsmallest(benchmark, dataframe, ncol_sort, n):
+    by = list(dataframe.columns[:ncol_sort])
+    benchmark(dataframe.nsmallest, n, by)
