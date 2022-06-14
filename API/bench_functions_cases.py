998879fdbd47ed3e3f55290ccebadb0f8856951b
Vyas Ramasubramani <vyasr@nvidia.com>
2022-06-14 10:26:00 -0700

    Generate concat data on device for improved performance.

--- API/bench_functions_cases.py
+++ API/bench_functions_cases.py
@@ -1,21 +1,21 @@
 import pytest_cases
-from config import NUM_ROWS, cudf
+from config import NUM_ROWS, cudf, cupy
 
 
 @pytest_cases.parametrize("nr", NUM_ROWS)
 def concat_case_1(nr):
     return [
-        cudf.DataFrame({"a": [1, 2, 3] * nr}),
-        cudf.DataFrame({"b": [4, 5, 7] * nr}),
+        cudf.DataFrame({"a": cupy.tile([1, 2, 3], nr)}),
+        cudf.DataFrame({"b": cupy.tile([4, 5, 7], nr)}),
     ]
 
 
 @pytest_cases.parametrize("nr", NUM_ROWS)
 def concat_case_2(nr):
     return [
-        cudf.DataFrame({"a": [1, 2, 3] * nr}),
+        cudf.DataFrame({"a": cupy.tile([1, 2, 3], nr)}),
         cudf.DataFrame(
-            {"b": [4, 5, 7] * nr},
+            {"b": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=nr * 3, stop=nr * 2 * 3),
         ),
     ]
@@ -24,9 +24,9 @@ def concat_case_2(nr):
 @pytest_cases.parametrize("nr", NUM_ROWS)
 def concat_case_3(nr):
     return [
-        cudf.DataFrame({"a": [1, 2, 3] * nr, "b": [4, 5, 7] * nr}),
+        cudf.DataFrame({"a": cupy.tile([1, 2, 3], nr), "b": cupy.tile([4, 5, 7], nr)}),
         cudf.DataFrame(
-            {"c": [4, 5, 7] * nr},
+            {"c": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=nr * 3, stop=nr * 2 * 3),
         ),
     ]
@@ -36,7 +36,7 @@ def concat_case_3(nr):
 def concat_case_4(nr):
     return [
         cudf.DataFrame(
-            {"a": [1, 2, 3] * nr, "b": [4, 5, 7] * nr},
+            {"a": cupy.tile([1, 2, 3], nr), "b": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=0, stop=nr * 3).astype("str"),
         ),
         cudf.DataFrame(
@@ -50,11 +50,11 @@ def concat_case_4(nr):
 def concat_case_5(nr):
     return [
         cudf.DataFrame(
-            {"a": [1, 2, 3] * nr, "b": [4, 5, 7] * nr},
+            {"a": cupy.tile([1, 2, 3], nr), "b": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=0, stop=nr * 3).astype("str"),
         ),
         cudf.DataFrame(
-            {"c": [4, 5, 7] * nr},
+            {"c": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=nr * 3, stop=nr * 2 * 3).astype("str"),
         ),
     ]
@@ -64,27 +64,27 @@ def concat_case_5(nr):
 def concat_case_6(nr):
     return [
         cudf.DataFrame(
-            {"a": [1, 2, 3] * nr, "b": [4, 5, 7] * nr},
+            {"a": cupy.tile([1, 2, 3], nr), "b": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=0, stop=nr * 3).astype("str"),
         ),
         cudf.DataFrame(
-            {"c": [4, 5, 7] * nr},
+            {"c": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=nr * 3, stop=nr * 2 * 3).astype("str"),
         ),
         cudf.DataFrame(
-            {"d": [1, 2, 3] * nr, "e": [4, 5, 7] * nr},
+            {"d": cupy.tile([1, 2, 3], nr), "e": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=0, stop=nr * 3).astype("str"),
         ),
         cudf.DataFrame(
-            {"f": [4, 5, 7] * nr},
+            {"f": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=nr * 3, stop=nr * 2 * 3).astype("str"),
         ),
         cudf.DataFrame(
-            {"g": [1, 2, 3] * nr, "h": [4, 5, 7] * nr},
+            {"g": cupy.tile([1, 2, 3], nr), "h": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=0, stop=nr * 3).astype("str"),
         ),
         cudf.DataFrame(
-            {"i": [4, 5, 7] * nr},
+            {"i": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=nr * 3, stop=nr * 2 * 3).astype("str"),
         ),
     ]
@@ -95,40 +95,40 @@ def concat_case_7(nr):
     # To avoid any edge case bugs, always use at least 10 rows per DataFrame.
     nr_actual = max(10, nr // 20)
     return [
-        cudf.DataFrame({"a": [1, 2, 3] * nr_actual}),
-        cudf.DataFrame({"b": [4, 5, 7] * nr_actual}),
-        cudf.DataFrame({"c": [1, 2, 3] * nr_actual}),
-        cudf.DataFrame({"d": [4, 5, 7] * nr_actual}),
-        cudf.DataFrame({"e": [1, 2, 3] * nr_actual}),
-        cudf.DataFrame({"f": [4, 5, 7] * nr_actual}),
-        cudf.DataFrame({"g": [1, 2, 3] * nr_actual}),
-        cudf.DataFrame({"h": [4, 5, 7] * nr_actual}),
-        cudf.DataFrame({"i": [1, 2, 3] * nr_actual}),
-        cudf.DataFrame({"j": [4, 5, 7] * nr_actual}),
+        cudf.DataFrame({"a": cupy.tile([1, 2, 3], nr_actual)}),
+        cudf.DataFrame({"b": cupy.tile([4, 5, 7], nr_actual)}),
+        cudf.DataFrame({"c": cupy.tile([1, 2, 3], nr_actual)}),
+        cudf.DataFrame({"d": cupy.tile([4, 5, 7], nr_actual)}),
+        cudf.DataFrame({"e": cupy.tile([1, 2, 3], nr_actual)}),
+        cudf.DataFrame({"f": cupy.tile([4, 5, 7], nr_actual)}),
+        cudf.DataFrame({"g": cupy.tile([1, 2, 3], nr_actual)}),
+        cudf.DataFrame({"h": cupy.tile([4, 5, 7], nr_actual)}),
+        cudf.DataFrame({"i": cupy.tile([1, 2, 3], nr_actual)}),
+        cudf.DataFrame({"j": cupy.tile([4, 5, 7], nr_actual)}),
     ]
 
 
 @pytest_cases.parametrize("nr", NUM_ROWS)
 def concat_case_8(nr):
     return [
-        cudf.DataFrame({"a": [1, 2, 3] * nr, "b": [4, 5, 7] * nr}),
+        cudf.DataFrame({"a": cupy.tile([1, 2, 3], nr), "b": cupy.tile([4, 5, 7], nr)}),
         cudf.DataFrame(
-            {"c": [4, 5, 7] * nr},
+            {"c": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=nr * 3, stop=nr * 2 * 3),
         ),
-        cudf.DataFrame({"d": [1, 2, 3] * nr, "e": [4, 5, 7] * nr}),
+        cudf.DataFrame({"d": cupy.tile([1, 2, 3], nr), "e": cupy.tile([4, 5, 7], nr)}),
         cudf.DataFrame(
-            {"f": [4, 5, 7] * nr},
+            {"f": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=nr * 3, stop=nr * 2 * 3),
         ),
-        cudf.DataFrame({"g": [1, 2, 3] * nr, "h": [4, 5, 7] * nr}),
+        cudf.DataFrame({"g": cupy.tile([1, 2, 3], nr), "h": cupy.tile([4, 5, 7], nr)}),
         cudf.DataFrame(
-            {"i": [4, 5, 7] * nr},
+            {"i": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=nr * 3, stop=nr * 2 * 3),
         ),
-        cudf.DataFrame({"j": [1, 2, 3] * nr, "k": [4, 5, 7] * nr}),
+        cudf.DataFrame({"j": cupy.tile([1, 2, 3], nr), "k": cupy.tile([4, 5, 7], nr)}),
         cudf.DataFrame(
-            {"l": [4, 5, 7] * nr},
+            {"l": cupy.tile([4, 5, 7], nr)},
             index=cudf.RangeIndex(start=nr * 3, stop=nr * 2 * 3),
         ),
     ]
